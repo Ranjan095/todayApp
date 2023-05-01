015f13a4b9eace64b2776f8f6e5fa2b16db7320e
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2023-05-01 22:50:39 +0530

    update

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
+const dotenv = require('dotenv')
 
-const connection =mongoose.connect("mongodb://127.0.0.1:27017/microSoft");
+const connection =mongoose.connect(process.env.mongoDB);
 
 module.exports={connection}
